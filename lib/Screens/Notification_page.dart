e2eefaa18f17cc822991cc41f535182b65f8852d
Howlbeeb <balogunhabeeb4@gmail.com>
2023-05-26 23:58:49 +0100

    Acute university new developments

--- lib/Screens/Notification_page.dart
+++ lib/Screens/Notification_page.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:flutter_svg/flutter_svg.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -12,8 +11,59 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image(
-        image: SvgPicture.asset(''),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Notifications",
+                  style: TextStyle(
+                    color: Color(0xff004ADD),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                Icon(
+                  Icons.close,
+                  size: 12,
+                  color: Color(0xff004ADD),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 19,
+            ),
+            Container(
+              height: 44,
+              decoration: BoxDecoration(
+                color: Color(0xFFF2F2F2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.search,
+                    size: 24,
+                    color: Color(0xFF000F2C4D).withOpacity(0.3),
+                  ),
+                  SizedBox(width: 13),
+                  Text(
+                    "Search Notifications",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF000F2C4D).withOpacity(0.3),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
